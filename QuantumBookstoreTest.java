ef851da1b8c981b58275c568eadee4cb2ff1fa49
Mohamed Ahmed Farag <mo.a.farag.122@gmail.com>
2025-07-08 23:07:55 +0300

    Update QuantumBookstoreTest.java

--- QuantumBookstoreTest.java
+++ QuantumBookstoreTest.java
@@ -5,8 +5,8 @@ public class QuantumBookstoreTest{
         MailService mailService= new SimpleMailService();
         Inventory inventory= new Inventory(shippingService, mailService);
 
-        Book paper= new PaperBook("ISBN11", "Java Tips", "Mark", 2013, 300, 10);
-        Book ebook= new EBook("ISBN22", "Database", "Ramy", 2020, 200, ".pdf");
+        Book paper= new PaperBook("ISBN11", "Java Tips", "Mark", 2010, 300, 2);
+        Book ebook= new EBook("ISBN22", "Database", "Ramy", 2010, 200, ".pdf");
         Book demo= new DemoBook("ISBN33", "C++ Basics", "Samuel", 2009);
 
         inventory.addBook(paper);
@@ -18,13 +18,22 @@ public class QuantumBookstoreTest{
         try {
             double amountPaid = inventory.buyBook("ISBN11", 2, "user@example.com", "Cairo, Egypt");
             System.out.println("Quantum book store: paid amount: " + amountPaid);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
-            amountPaid = inventory.buyBook("ISBN22", 1, "user@example.com", "N/A");
+        try {
+            double amountPaid = inventory.buyBook("ISBN22", 1, "user@example.com", "N/A");
             System.out.println("Quantum book store: paid amount: " + amountPaid);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
+        try {
             inventory.buyBook("ISBN33", 1, "user@example.com", "N/A");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+
     }
 }
